d7bc0a5b4432467e457b212d13038562aaf56f0e
mernaehab404 <118836442+mernaehab404@users.noreply.github.com>
2023-05-09 18:48:16 +0300

    Delete src directory

--- src/middleware/Admin.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { getAuthUser } from '../helper/Storage';
-import { Navigate, Outlet } from 'react-router-dom';
-
-const Guest= () =>{
-    const auth = getAuthUser();
-    return <> {auth && auth.type === 1  ? <Outlet/> : <Navigate to ={"/"} /> }</>
-};
-export default Guest;
--- src/middleware/Guest.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { getAuthUser } from '../helper/Storage';
-import { Navigate, Outlet } from 'react-router-dom';
-
-const Guest= () =>{
-    const auth = getAuthUser();
-    return <> {!auth  ? <Outlet/> : <Navigate to ={"/"} /> }</>
-};
-export default Guest;
