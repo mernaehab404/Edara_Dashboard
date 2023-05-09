3dd67c1bd5866364d6e29043747e61a08c3c46bd
NADAMAHer74 <120984903+NADAMAHer74@users.noreply.github.com>
2023-05-09 21:42:31 +0300

    Add files via upload

---
+++ Read_sup.js
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import { useParams, Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function Read_sup(){
+    const {id}= useParams();
+    const [users, setUsers]= useState([])
+    useEffect(()=>{
+        axios.get('http://localhost:8081/read/'+id)
+        .then(res => {
+            console.log(res)
+            setUsers(res.data[0]);
+        })
+        .catch(err => console.log(err))
+
+    },[])
+
+
+
+
+    return(
+        <Card style={{ width: '30rem' }}  className="m-5">
+        <Card.Body>
+        <Card.Title>Supervisor Detail</Card.Title>
+        <Card.Text>
+           <h2>ID    : {users.id} </h2>
+           <h2>Email : {users.email}</h2>
+           <h2>{users.password}</h2>
+           <h2>{users.phone}</h2>
+           <h2>{users.status}</h2>
+           <h2>{users.type}</h2>
+           <h2>{users.name}</h2>
+        </Card.Text>
+        <Link to="/homess" >
+            <Button variant="dark">back</Button></Link>
+      </Card.Body>
+    </Card>
+        
+
+         
+
+           
+
+           
+           
+          
+    );
+}
+
+export default Read_sup;
