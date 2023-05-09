7c76056ed154dc67982cde10094df6a2699ee2ad
mona-gamal <119448237+mona-gamal@users.noreply.github.com>
2023-05-09 18:48:52 +0300

    Add files via upload

---
+++ middleware/Guest.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { getAuthUser } from '../helper/Storage';
+import { Navigate, Outlet } from 'react-router-dom';
+
+const Guest= () =>{
+    const auth = getAuthUser();
+    return <> {!auth  ? <Outlet/> : <Navigate to ={"/"} /> }</>
+};
+export default Guest;
