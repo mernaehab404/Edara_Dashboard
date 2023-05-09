db1d05eb11126d287f8947556ecbc7821771abc1
mernaehab404 <118836442+mernaehab404@users.noreply.github.com>
2023-05-09 22:12:16 +0300

    Add files via upload

---
+++ NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const NotFound = () => {
+    return ( 
+        <h1 style={{textAlign:'center',margin:"25%",color:"rgb(247, 246, 240)",}}>Error 404 Page Not Found !!</h1>
+     );
+}
+ 
+export default NotFound;
